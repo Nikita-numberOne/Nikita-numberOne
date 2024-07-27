8df7ddc3116ac3167577d59e9cfb41ec1f210066
Nikita-numberOne <jasonzhora@mail.ru>
2024-07-27 17:32:07 +0300

    Add files via upload
    Практическое задание по уроку "Базовые структуры данных"

---
+++ task1.py
@@ -0,0 +1,10 @@
+
+"1st program"
+print((9 ** 0.5) * 5)
+"2nd program"
+print((99.9 > 99.8) and (1000 <= 1001))
+"3rd program"
+print((( 1234 % 1000 ) // 10) + (5678 % 1000) // 10)
+"4th program"
+print((int(13.42) == int(42.13 * 100 % 100)) or (int(42.13) == int(13.42 * 100 % 100)))
+
